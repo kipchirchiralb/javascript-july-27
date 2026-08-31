f967126b718081958e73f7e8f392118a6710b5a3
Albert <albertkipchirchir@gmail.com>
2026-08-31 15:24:37 +0300

    learning programming fundamemtals

---
+++ hello.js
@@ -0,0 +1,23 @@
+console.log("Hello World "); // prints hello world on terminal/console
+
+console.log("jersey");
+
+// variables , data types, operators, functions
+// software - instructions/steps(tasks) + data
+
+// A variable is a way to store/hold data in memory/RAM -
+// a database stores data in Storage(ROM , SSD, HDD)
+
+// in javascript  , we decalre varaibles using let or const
+// let for varaibles that changes  as application runs
+// const for varables that are constant/do not change as application is running
+
+let score = 20;
+score = 21;
+score = 25;
+
+const pi = 3.142;
+const interestRate = 14;
+// we can log varibales/access their values
+console.log(score)
+console.log(interestRate)
